f5531b2c07e065d2d7ea6fcd9a07382ec48ed45b
Leonardo Kazuyoshi <54998411+leonardo-kazu@users.noreply.github.com>
2023-03-08 20:46:46 -0400

    feat(baseclock): created the base clock functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+import { useEffect, useState } from 'react';
 import Clock from './components/Clock';
 
 function App() {
+  const [pomodoroTime, setPomodoroTime] = useState<number>(60 * 25);
+  const [runPomodoro, setRunPomodoro] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (runPomodoro) {
+      setTimeout(() => {
+        setPomodoroTime((time) => time - 1);
+      }, 1000);
+    }
+  }, [pomodoroTime]);
+
   return (
     <div className="App">
-      <Clock time={10} title={'Pomodoro time'}></Clock>
+      <Clock time={pomodoroTime} title={'Pomodoro Time Left'} />
     </div>
   );
 }
