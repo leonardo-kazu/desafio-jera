d591c5c0f582d181d2a572f96db19fac1d621f20
Leonardo Kazuyoshi <54998411+leonardo-kazu@users.noreply.github.com>
2023-03-09 13:27:19 -0400

    feat(app): reseted all the timer logic to follow the correct scope
    We were following the wrong scope, because of bad text interpretation, it should be okay right now.

--- src/App.tsx
+++ src/App.tsx
@@ -4,80 +4,50 @@ import Clock from './components/Clock';
 function App() {
   const [timesRun, setTimesRun] = useState<number>(0);
 
-  const [pomodoro, setPomodoro] = useState<number>(60 * 25);
+  const [pomodoro, setPomodoro] = useState<number>(60 * 10);
   const [stop, setStop] = useState(60 * 5);
 
-  const [pomodoroTimer, setPomodoroTimer] = useState<number>(pomodoro);
+  const [timer, setTimer] = useState<number>(pomodoro);
   const [runPomodoro, setRunPomodoro] = useState<boolean>(false);
-  const [stopTimer, setStopTimer] = useState<number>(stop);
   const [stopActive, setStopActive] = useState<boolean>(false);
-  const [runStop, setRunStop] = useState<boolean>(false);
+  const [isPomodoro, setIsPomodoro] = useState<boolean>(true);
 
-  // Use effect for pomodoro timer
   useEffect(() => {
-    // Checking if the pomodoro has been initiated and the stop timer isn't
-    // running
-    if (runPomodoro && !runStop) {
-      // If the timer has ended reset it and start the stop timer
-      if (pomodoroTimer === 0) {
-        setPomodoroTimer(pomodoro);
-
-        // Adding the amounts it has ran
-        setTimesRun((time) => time + 1);
-
-        // If the stop timer is active enable it
-        if (stopActive) {
-          setRunStop(true);
+    // Check if should start the timer
+    if (runPomodoro) {
+      // If the timer is zero run the logic for changing or keep on counting down
+      if (timer === 0) {
+        // Check if is running the pomodoro clock then sum the times ran
+        if (isPomodoro) {
+          setTimesRun((ran) => ran + 1);
+          // If we should ran the stop timer set pomodoro to false or set back to pomodoro
+          if (stopActive) setIsPomodoro(false);
+        } else {
+          setIsPomodoro(true);
         }
 
-        // Stop the timer
-        setRunPomodoro(false);
+        // Check if we need to start the stop timer or reset pomodoro
+        if (stopActive && !isPomodoro) {
+          setTimer(stop);
+        } else {
+          setTimer(pomodoro);
+        }
       } else {
-        // Main timer logic
         setTimeout(() => {
-          setPomodoroTimer((time) => time - 1);
-        }, 1);
-      }
-    } else {
-      // Reset the timer
-      setPomodoroTimer(pomodoro);
-    }
-  }, [pomodoroTimer, runPomodoro, runStop]);
-
-  // Use effect for the stop timer
-  useEffect(() => {
-    // Checking if the stop timer should run and is active
-    if (runStop && stopActive) {
-      // If the timer has ended, reset it and start the pomodoro
-      if (stopTimer === 0) {
-        setStopTimer(stop);
-        setRunPomodoro(true);
-
-        // Stop the timer
-        setRunStop(false);
-      } else {
-        // Main timer logic
-        setTimeout(() => {
-          setStopTimer((time) => time - 1);
+          setTimer((time) => time - 1);
         }, 1);
       }
     }
-  }, [runStop, stopTimer, runPomodoro]);
+  }, [timer, runPomodoro, isPomodoro, stopActive]);
 
   return (
     <div className="App">
-      <label htmlFor="useBreak">
-        <input
-          type="checkbox"
-          name="useBreak"
-          id="useBreak"
-          onChange={() => setStopActive((active) => !active)}
-        />
+      <label htmlFor="useStop">
+        <input type="checkbox" id="useStop" onChange={() => setStopActive((active) => !active)} />
         Do you want to use a break timer?
       </label>
       <p>Times pomodoro ran: {timesRun}</p>
-      <Clock time={pomodoroTimer} title={'Pomodoro Time Left'} />
-      {stopActive ? <Clock time={stopTimer} title={'Break Time Left'} /> : <></>}
+      <Clock time={timer} title={'Pomodoro Time Left'} />
       <button onClick={() => setRunPomodoro(true)}>Start</button>
     </div>
   );
