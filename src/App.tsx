6a06322680c1247a7b7ca5e0cd01ab3f516873f8
Leonardo Kazuyoshi <54998411+leonardo-kazu@users.noreply.github.com>
2023-03-09 10:52:49 -0400

    feat(basetimer): added base timer functionalities and stop timer option

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,64 @@ import { useEffect, useState } from 'react';
 import Clock from './components/Clock';
 
 function App() {
-  const [pomodoroTime, setPomodoroTime] = useState<number>(60 * 25);
+  const [timesRun, setTimesRun] = useState<number>(0);
+
+  const [pomodoro, setPomodoro] = useState<number>(60 * 25);
+  const [stop, setStop] = useState(60 * 5);
+
+  const [pomodoroTimer, setPomodoroTimer] = useState<number>(pomodoro);
   const [runPomodoro, setRunPomodoro] = useState<boolean>(false);
+  const [stopTimer, setStopTimer] = useState<number>(stop);
+  const [stopActive, setStopActive] = useState<boolean>(false);
+  const [runStop, setRunStop] = useState<boolean>(false);
 
   useEffect(() => {
     if (runPomodoro) {
+      if (pomodoroTimer === 0) {
+        setPomodoroTimer(pomodoro);
+        setTimesRun((time) => time + 1);
+        if (stopActive) {
+          setRunStop(true);
+        }
+        setRunPomodoro(false);
+      }
       setTimeout(() => {
-        setPomodoroTime((time) => time - 1);
-      }, 1000);
+        setPomodoroTimer((time) => time - 1);
+      }, 1);
+    } else {
+      setPomodoroTimer(pomodoro);
     }
-  }, [pomodoroTime]);
+  }, [pomodoroTimer, runPomodoro, stopActive]);
+
+  useEffect(() => {
+    if (runStop && stopActive) {
+      if (stopTimer === 0) {
+        setRunPomodoro(true);
+        setStopTimer(stop);
+        setRunStop(false);
+      } else {
+        setTimeout(() => {
+          setStopTimer((time) => time - 1);
+        }, 1);
+      }
+    }
+  }, [runStop, stopTimer, runPomodoro]);
 
   return (
     <div className="App">
-      <Clock time={pomodoroTime} title={'Pomodoro Time Left'} />
+      <label htmlFor="useBreak">
+        <input
+          type="checkbox"
+          name="useBreak"
+          id="useBreak"
+          onChange={() => setStopActive((active) => !active)}
+        />
+        Do you want to use a break timer?
+      </label>
+      <p>Times pomodoro ran: {timesRun}</p>
+      <Clock time={pomodoroTimer} title={'Pomodoro Time Left'} />
+      {stopActive ? <Clock time={stopTimer} title={'Break Time Left'} /> : <></>}
+      <button onClick={() => setRunPomodoro(true)}>Start</button>
     </div>
   );
 }
